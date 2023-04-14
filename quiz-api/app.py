5d1c49e08d1e4e3f26759f16929d08bf21bdd4a0
rayen.soltani <rayen.soltani@edu.esiee.fr>
2023-04-14 11:38:00 +0200

    route /quiz-info

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -2,11 +2,19 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def hello_world():
 	x = 'world'
 	return f"Hello, {x}, it's rayen"
+
+@app.route('/quiz-info', methods=['GET'])
+def GetQuizInfo():
+	return {"size": 0, "scores": []}, 200
+
+
+
+
 
 if __name__ == "__main__":
     app.run()
